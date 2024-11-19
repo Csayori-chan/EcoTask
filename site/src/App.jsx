5b64e17fc31fd7045f81a732a224e4b59322d2d5
Davi dos Santos Passos <69681778+pessoa736@users.noreply.github.com>
2024-11-19 15:42:06 +0000

    update

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
 import Global from "./styles/Global";
+import Background from "./components/background"
 
 
 function App() {
   return (
     <div>
+      <Background />
       <Header />
       <Global />
     </div>
---
+++ site/src/components/Background/index.jsx
@@ -0,0 +1,11 @@
+import Cover_img from "../../assets/cover.png"
+
+const Backgrond = () => {
+    return (
+        <img scr={Cover_img} style={{
+            zIndex: 0
+        }}/>
+    )
+}
+
+export default Backgrond;
